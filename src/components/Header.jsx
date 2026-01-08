31832914a1c3c27d1780c21b409e5489dca21e03
Arkin26 <arkinutmal@gmail.com>
2026-01-08 19:51:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,6 +32,8 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { FaDiscord, FaTwitter, FaInstagram, FaTwitch } from 'react-icons/fa';
 
+const testError = undefinedVariable;
+
 export default function Header() {
  
 
