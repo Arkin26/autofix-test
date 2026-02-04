9a36873bd6464411aa284fb21cd49e9604551561
Arkin26 <arkinutmal@gmail.com>
2026-02-04 07:05:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// import Header from './components/Header';
-// import Slideshow from './components/Slideshow';
-// import LogoMarquee from './components/LogoMarquee';
-// import HoverCards from './components/HoverCards';
-// import MerchShowcase from './components/MerchShowcase';
-// import Footer from './components/Footer';
-
-// import CS2Page from "./components/Team-pages/CS2";
-// import RivalsPage from "./components/Team-pages/Marvel-Rivals";
-// import R6Page from "./components/Team-pages/R6";
-// import RocketLeaguePage from "./components/Team-pages/Rocket-League";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Home Page */}
-//         <Route
-//           path="/"
-//           element={
-//             <>
-//               <Header />
-//               <Slideshow />
-//               <LogoMarquee />
-//               <HoverCards />
-//               <MerchShowcase />
-//               <div className="w-full h-[2px] bg-[#4DA0C7]" />
-//               <Footer />
-//             </>
-//           }
-//         />
-
-//         {/* Game Pages */}
-//         <Route path="/cs2" element={<CS2Page />} />
-//         <Route path="/marvel-rivals" element={<RivalsPage />} />
-//         <Route path="/r6" element={<R6Page />} />
-//         <Route path="/rocket-league" element={<RocketLeaguePage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { NonExistentComponent } from './this-file-does-not-exist';
 
 
 import Header from './components/Header';
